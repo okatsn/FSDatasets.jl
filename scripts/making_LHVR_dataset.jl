d0eb1786fc0022c40fdaebed33b801ce7a2d15b7
okatsn <okatsn@gmail.com>
2023-03-03 11:44:58 +0800

    Working on dataframe

--- scripts/making_LHVR_dataset.jl
+++ scripts/making_LHVR_dataset.jl
@@ -1,2 +1,17 @@
-using DataFrames, CSV
+using DataFrames, CSV, OkFiles, Chain
 using Revise, FSDatasets, SmallDatasetMaker
+
+
+
+flist = filelist(r"FS\_FrictionCoeff\_fulltable\_s\d+\.csv", FSDatasets.carbdatadir())
+
+dfs = CSV.read.(flist, DataFrame)
+describe.(dfs)
+
+df1 = dfs[1]
+
+dflong = @chain df1 begin 
+    # stack(Cols(r"(SEP|FIM)"))
+    select()
+    transform(:variable => ByRow(s -> parse(Int, match(r"\d+\Z", s).match)) => "Moving window")
+end
--- src/projectdir.jl
+++ src/projectdir.jl
@@ -1,5 +1,5 @@
 
 # Directories for the dataset-creating stage; do not export any of them.
-rawdatadir(args...) = projectdir("data", "raw")
-carbdatadir(args...) = rawdatadir("Carbonate_Elena_21")
-silidatadir(args...) = rawdatadir("Silicate_LiWei_21")
+rawdatadir(args...) = projectdir("data", "raw", args...)
+carbdatadir(args...) = rawdatadir("Carbonate_Elena_21", args...)
+silidatadir(args...) = rawdatadir("Silicate_LiWei_21", args...)
